626ed611a8100075130960c509ac35a5e07173b0
AveMrtn <avejanica.moratin@gmail.com>
2017-07-07 17:45:28 +0800

    dsa

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, 'views/index.html'));
 });
-
+app.get('/ElectiveAssign', function(req, res){
+  res.sendFile(path.join(__dirname, 'views/ElectiveAssign.html'));
+});
 //express server listen
 var server = app.listen(app.get('port'), function(){
   console.log('Server listening on port ',app.get('port'));
